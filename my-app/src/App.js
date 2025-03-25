44ba987df21431a70670e704e531e9484d5992bf
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-03-25 23:57:03 +0300

    added an image section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import AvatarGroup from '@mui/material/AvatarGroup';
 import Avatar from '@mui/material/Avatar';
+import ImageList from '@mui/material/ImageList';
+import { ImageListItem } from '@mui/material';
 
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
+    <div className="row">
+    <div className="col-sm-12"> 
   
   <AvatarGroup total={10}>
   <Avatar alt="Remy Sharp" src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/159/904/original/Vegetarian_Vegan_Waakye_Stew_-_The_Canadian_African.jpeg?1741710202" />
@@ -15,7 +19,28 @@ function App() {
   <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
   <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
 </AvatarGroup>
+</div>   
 
+
+<div className="col-sm-12">
+<ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+  {itemData.map((item) => (
+    <ImageListItem key={item.img}>
+      <img
+        srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+        src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+        alt={item.title}
+        loading="lazy"
+      />
+    </ImageListItem>
+  ))}
+</ImageList>
+</div>
+
+
+
+
+</div>
 
     </div>
   );
