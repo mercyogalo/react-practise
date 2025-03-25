f536e4bd93da6d41c47588930e001b8d9ef91e01
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-03-26 00:19:03 +0300

    updated the image section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,77 @@ import { ImageListItem } from '@mui/material';
 
 
 
+
 function App() {
+
+
+
+
+
+  const itemData = [
+    {
+      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      title: 'Breakfast',
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+      title: 'Burger',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+      title: 'Camera',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+      title: 'Coffee',
+      cols: 2,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+      title: 'Hats',
+      cols: 2,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+      title: 'Honey',
+      author: '@arwinneil',
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+      title: 'Basketball',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+      title: 'Fern',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+      title: 'Mushrooms',
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+      title: 'Tomato basil',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+      title: 'Sea star',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+      title: 'Bike',
+      cols: 2,
+    },
+  ];
+ 
+    
+
+
   return (
     <div className="App container">
     <div className="row">
@@ -23,7 +93,9 @@ function App() {
 
 
 <div className="col-sm-12">
-<ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+
+
+<ImageList sx={{ width: 500, height: 450 }} cols={4} rowHeight={164}>
   {itemData.map((item) => (
     <ImageListItem key={item.img}>
       <img
@@ -35,6 +107,7 @@ function App() {
     </ImageListItem>
   ))}
 </ImageList>
+
 </div>
 
 
