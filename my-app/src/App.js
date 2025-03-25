97cd3fcc6df70161137a04682e0afc1ba78df93c
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-03-25 23:44:03 +0300

    used the avatars
    how they are set up and layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import AvatarGroup from '@mui/material/AvatarGroup';
+import Avatar from '@mui/material/Avatar';
+
+
 
 function App() {
   return (
     <div className="App">
-    <p>Hello world</p>
+  
+  <AvatarGroup total={10}>
+  <Avatar alt="Remy Sharp" src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/159/904/original/Vegetarian_Vegan_Waakye_Stew_-_The_Canadian_African.jpeg?1741710202" />
+  <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+  <Avatar alt="Cindy Baker" src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/146/722/original/Screenshot_2024-10-08_001547.png?1728335801" />
+  <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+  <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+</AvatarGroup>
+
+
     </div>
   );
 }
