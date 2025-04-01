5aad43a57c09d0c395e51fd7209d88afbd02c511
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-04-01 17:35:57 +0300

    updated the styling for the about section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import AvatarGroup from '@mui/material/AvatarGroup';
-import Avatar from '@mui/material/Avatar';
 import { Typography} from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -114,25 +112,15 @@ function App() {
 
 
   return (
-    <div className="row">
+    <div className="row mt-3">
 
 
 
 
 
-    <div className="col-sm-12"> 
-  
-  <AvatarGroup total={10}>
-  <Avatar alt="Remy Sharp" src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/159/904/original/Vegetarian_Vegan_Waakye_Stew_-_The_Canadian_African.jpeg?1741710202" />
-  <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-  <Avatar alt="Cindy Baker" src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/146/722/original/Screenshot_2024-10-08_001547.png?1728335801" />
-  <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
-  <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
-</AvatarGroup>
-</div>   
 
 
-<div className="col-sm-12">
+<div className="col-sm-12 mt-3">
   <Slide_show />
 </div>
 
